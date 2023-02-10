458f307b8e2e6f3b134126b69d251227201db614
ricbaiano <ricbaiano@gmail.com>
2023-02-10 14:21:13 -0300

    feat:Inclusao de Mensagem 6 no sistema

--- Program.cs
+++ Program.cs
@@ -5,3 +5,4 @@ Console.WriteLine("Nova Mensagem no console 2!");
 Console.WriteLine("Nova Mensagem no console 3!");
 Console.WriteLine("Nova Mensagem no console 4!");
 Console.WriteLine("Nova Mensagem no console 5!");
+Console.WriteLine("Nova Mensagem no console 6!");
