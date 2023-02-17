aba23fefe8be93745a87fe228f4d0f35b86ebba0
ricbaiano <ricbaiano@gmail.com>
2023-02-17 14:25:41 -0300

    feat(TESTEJJ-6): Nova tela 23

--- src/Program.cs
+++ src/Program.cs
@@ -6,3 +6,4 @@ Console.WriteLine("Nova Mensagem no console 3!");
 Console.WriteLine("Nova Mensagem no console 20!");
 Console.WriteLine("Nova Mensagem no console 21!");
 Console.WriteLine("Nova Mensagem no console 22!");
+Console.WriteLine("Nova Mensagem no console 23!");
